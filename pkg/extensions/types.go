add9575e34f2a332100c7a0f345bc65b628b1646
Malte Isberner <malte@redhat.com>
2021-06-08 02:42:37 +0200

    pointer

--- pkg/extensions/types.go
+++ pkg/extensions/types.go
@@ -8,4 +8,4 @@ import (
 
 // ReconcileExtension is an arbitrary extension that can be implemented to run either before
 // or after the main Helm reconciliation action.
-type ReconcileExtension func(context.Context, unstructured.Unstructured, logr.Logger) error
+type ReconcileExtension func(context.Context, *unstructured.Unstructured, logr.Logger) error
