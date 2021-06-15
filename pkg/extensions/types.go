a30b47c52cfd045e8c074a98265970d9e3b885d7
Malte Isberner <2822367+misberner@users.noreply.github.com>
2021-06-15 12:03:12 +0200

    Update pkg/extensions/types.go
    Co-authored-by: Marcin Owsiany <marcin@stackrox.com>

--- pkg/extensions/types.go
+++ pkg/extensions/types.go
@@ -8,4 +8,5 @@ import (
 
 // ReconcileExtension is an arbitrary extension that can be implemented to run either before
 // or after the main Helm reconciliation action.
+// An error returned by a ReconcileExtension will cause the Reconcile to fail, unlike a hook error.
 type ReconcileExtension func(context.Context, *unstructured.Unstructured, logr.Logger) error
