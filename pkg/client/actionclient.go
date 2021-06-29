ac3969587d768d5cec3d1bab4c455005d31c5fb0
Malte Isberner <2822367+misberner@users.noreply.github.com>
2021-06-29 15:16:01 +0200

    Allow marking releases stuck in a pending state as failed (#16)

--- pkg/client/actionclient.go
+++ pkg/client/actionclient.go
@@ -54,6 +54,7 @@ type ActionInterface interface {
 	Get(name string, opts ...GetOption) (*release.Release, error)
 	Install(name, namespace string, chrt *chart.Chart, vals map[string]interface{}, opts ...InstallOption) (*release.Release, error)
 	Upgrade(name, namespace string, chrt *chart.Chart, vals map[string]interface{}, opts ...UpgradeOption) (*release.Release, error)
+	MarkFailed(release *release.Release, reason string) error
 	Uninstall(name string, opts ...UninstallOption) (*release.UninstallReleaseResponse, error)
 	Reconcile(rel *release.Release) error
 }
@@ -276,6 +277,14 @@ func (c *actionClient) Upgrade(name, namespace string, chrt *chart.Chart, vals m
 		return rel, err
 	}
 	return rel, nil
+}
+
+func (c *actionClient) MarkFailed(rel *release.Release, reason string) error {
+	infoCopy := *rel.Info
+	releaseCopy := *rel
+	releaseCopy.Info = &infoCopy
+	releaseCopy.SetStatus(release.StatusFailed, reason)
+	return c.conf.Releases.Update(&releaseCopy)
 }
 
 func (c *actionClient) rollback(name string, opts ...RollbackOption) error {
