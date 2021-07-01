eac78a5f5bc4f11d0e4fab2de78983b50c74af00
Malte Isberner <2822367+misberner@users.noreply.github.com>
2021-07-01 12:01:09 +0200

    Allow stripping manifest from the CR status (#18)

--- pkg/reconciler/reconciler_test.go
+++ pkg/reconciler/reconciler_test.go
@@ -198,6 +198,16 @@ var _ = Describe("Reconciler", func() {
 				Expect(r.skipDependentWatches).To(BeTrue())
 			})
 		})
+		_ = Describe("StripManifestFromStatus", func() {
+			It("should set to false", func() {
+				Expect(StripManifestFromStatus(false)(r)).To(Succeed())
+				Expect(r.stripManifestFromStatus).To(Equal(false))
+			})
+			It("should set to true", func() {
+				Expect(StripManifestFromStatus(true)(r)).To(Succeed())
+				Expect(r.stripManifestFromStatus).To(Equal(true))
+			})
+		})
 		_ = Describe("WithMaxConcurrentReconciles", func() {
 			It("should set the reconciler max concurrent reconciled", func() {
 				Expect(WithMaxConcurrentReconciles(1)(r)).To(Succeed())
