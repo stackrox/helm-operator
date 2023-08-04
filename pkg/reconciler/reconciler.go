090c1105ecf3a070d0690fa305356a80df9646f9
Ludovic Cleroux <ludydoo@gmail.com>
2023-08-04 15:28:54 +0200

    Fix rebase

--- pkg/reconciler/reconciler.go
+++ pkg/reconciler/reconciler.go
@@ -1194,7 +1194,6 @@ func (r *Reconciler) ensureDeployedRelease(u *updater.Updater, rel *release.Rele
 		rel = &relCopy
 	}
 
-	u.Update(updater.EnsureFinalizer(uninstallFinalizer))
 	u.UpdateStatus(
 		updater.EnsureCondition(conditions.Deployed(corev1.ConditionTrue, reason, message)),
 		updater.EnsureDeployedRelease(rel),
